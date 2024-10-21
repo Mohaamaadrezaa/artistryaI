cf6ed3b266043c62ed81486be3b9316a4ad4f538
Mohamadreza <127484346+Mohaamaadrezaa@users.noreply.github.com>
2024-10-21 23:45:48 +0330

    Add i18next for internationalization; update routing and image links; add LoadingComponent * Add i18next, i18next-browser-languagedetector, i18next-http-backend

--- constants/i18n.ts
+++ constants/i18n.ts
@@ -6,7 +6,7 @@ i18n.use(initReactI18next).init({
   fallbackLng: "en",
   interpolation: {
     escapeValue: false, // not needed for react as it escapes by default
-  },
+  }, 
   // language resources
   resources: {
     en: {
